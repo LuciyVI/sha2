c7b15505761e0e03bdb1ab602a7c1a873349debf
LuciyVI <62550083+LuciyVI@users.noreply.github.com>
2021-09-28 21:24:27 +0300

    add_buff

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
-#include <iostream> 
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <vector>
 #include </home/dr3ad/repos/LychiVI/sha256/sha256.h>
+#include </home/dr3ad/repos/LychiVI/sha256/sha256.c>
 
 int main()
 {
-    
-   char data;
-   std::cin >> data;
-   
-   void sha256_init(struct sha256_buff* data);
-   void sha256_easy_hash(uint64_t* data);
-   void update(const void* data, std::size_t size);
-   std::cout<<data<<std::endl;
-   
-}
+sha256_buff();
+void sha256_init(struct sha256_buff* buff);
+const char* str = "test";
+char hash[65] = {0};
+sha256_read(struct sha256_buff* buff, uint8_t* hash);
+void sha256_init(struct sha256_buff* buff);
+sha256_easy_hash_hex(str, strlen(str), hash);
+printf("%s\n", hash);
+
+}   
